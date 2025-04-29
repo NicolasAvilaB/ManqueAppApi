1a4b6e7078069ecfa76dffd0fe9a7d96b2206f6f
nicolasavilab <nicolasab1996@gmail.com>
2025-04-28 23:21:56 -0400

    fix imports

--- src/main/kotlin/plugins/Routing.kt
+++ src/main/kotlin/plugins/Routing.kt
@@ -1,8 +1,8 @@
 package plugins
 
 import com.manque.app.presentation.routes.studentsTFC
-import io.ktor.server.application.*
-import io.ktor.server.routing.*
+import io.ktor.server.application.Application
+import io.ktor.server.routing.routing
 
 fun Application.configureRouting() {
     routing {
--- src/main/kotlin/presentation/routes/StudentsTFC.kt
+++ src/main/kotlin/presentation/routes/StudentsTFC.kt
@@ -8,10 +8,14 @@ import com.manque.app.data.model.Constants.MESSAGE_STUDENT_UPDATED_SUCCESS
 import data.DatabaseRepository
 import data.model.RemoteDataStudentsTfc
 import data.model.RemoteErrorMessage
-import io.ktor.http.*
-import io.ktor.server.request.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.request.receive
+import io.ktor.server.response.respond
+import io.ktor.server.routing.get
+import io.ktor.server.routing.put
+import io.ktor.server.routing.post
+import io.ktor.server.routing.delete
+import io.ktor.server.routing.Route
 
 fun Route.studentsTFC() {
     val databaseRepository = DatabaseRepository()
